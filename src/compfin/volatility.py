be2b1b91c91c27cafeed09afef4b93e4021a9e93
MarcellSzegedi <marcellvonszeged@gmail.com>
2025-04-05 14:14:45 +0200

    Changed volatility to lint and format to the standards

--- src/compfin/volatility.py
+++ src/compfin/volatility.py
@@ -1,12 +1,13 @@
+"""Market volatility calculations."""
+
+import yfinance as yf
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+from skfolio.datasets import load_sp500_dataset, load_sp500_implied_vol_dataset
+from skfolio.preprocessing import prices_to_returns
+
 """
-Course: Computational Finance
-Names: Marcell ..., Michael ... and Tika van Bennekum
-Student IDs: ..., ... and 13392425
-
-File description:
-    Part 1 of lab assignment 1.
-    ...
-
 Notes:
     tickers:
     "AAPL" is for Apple Inc.
@@ -15,16 +16,6 @@ Notes:
     "^GSPC" is for the S&P 500 index
     "TSLA" is for Tesla
 """
-
-import yfinance as yf
-import datetime
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-from skfolio.datasets import load_sp500_dataset, load_sp500_implied_vol_dataset
-from skfolio.preprocessing import prices_to_returns
-
-
 ticker = "TSLA"
 start_date = "2015-01-01"
 end_date = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -43,16 +34,17 @@ mean_return = returns.mean()
 volatility = returns.std()
 
 # alternative (parkinson) volatility
-inside_sum_parkinson = np.log(prices_high / prices_low)**2
+inside_sum_parkinson = np.log(prices_high / prices_low) ** 2
 constant_parkinson = 1 / 4 * np.log(2)
 volatility_parkinson = np.sqrt(constant_parkinson * inside_sum_parkinson.sum())
 
+
 def rolling_window_comparison():
+    """Compares the volatilities by applying rolling window."""
     size_window = 30
     rolling_window = returns.rolling(window=size_window).std()
     sum_parkinson_window = inside_sum_parkinson.rolling(window=size_window).sum()
     rolling_window_parkinson = np.sqrt(constant_parkinson * sum_parkinson_window)
-
 
     plt.figure(figsize=(12, 6))
     rolling_window.plot(label="Rolling volatility classical")
@@ -63,20 +55,17 @@ def rolling_window_comparison():
     plt.xlabel("Date")
     plt.show()
 
+
 # rolling_window_comparison()
 
-# volatility signature plot
-# Unsure what m stand for, wheter it means the size of the rolling window or something else
-# TA also doesn't know, so he will tell me tommorow.
-def signature_plot():
-    return
 
 def realized_vs_implied():
+    """Compares the realised and implied volatilities."""
     # Load historical price data and implied volatility data
     # TODO: results look weird, fix it
     # TODO: add the parkinson estimator
     prices = load_sp500_dataset()
-    stock = "GE" # GE is a random stock from S&P 500
+    stock = "GE"  # GE is a random stock from S&P 500
 
     implied_vol = load_sp500_implied_vol_dataset()["2015":]
     implied_vol = implied_vol[stock]
@@ -94,5 +83,5 @@ def realized_vs_implied():
     plt.xlabel("Date")
     plt.show()
 
-realized_vs_implied()
 
+realized_vs_implied()
