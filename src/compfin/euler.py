3b43fac96fbfc899ac42cc14ce0a8c0cb9ceebac
TikaBennekum <107108939+TikaBennekum@users.noreply.github.com>
2025-04-09 14:23:37 +0200

    euler

---
+++ src/compfin/euler.py
@@ -0,0 +1,40 @@
+# Consider a short position in a European call option on a non-dividend paying stock with a maturity of one year
+# and strike K = 99 EUR. Let the one-year risk-free interest rate be 6% and the current stock price be 100 EUR.
+# Furthermore, assume that the volatility is 20%.
+# Use the Euler method to perform a hedging simulation
+
+# Matching Volatility: Conduct an experiment where the volatility in the stock price process matches the
+# volatility used in the delta computation (i.e., both set to 20%). Vary the frequency of hedge adjustments
+# (from daily to weekly) and explain the results.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def simulate_euler_black_scholes(S0, mu, sigma, T, N):
+    """ taken from psuedocode in lecturenotes. """
+    dt = T / N  # Time step
+    time = np.linspace(0, T, N + 1)  # Time grid
+    S = np.zeros(len(time))
+    S[0] = S0
+
+    for t in range(1, len(time)):
+        dW = np.random.normal(0, np.sqrt(dt))  # Brownian motion increment
+        S[t] = S[t - 1] + mu * S[t - 1] * dt + sigma * S[t - 1] * dW  # Euler update
+
+    return time, S
+
+# Parameters
+S0 = 100     # Initial asset price
+mu = 0.05    # Drift
+sigma = 0.2  # Volatility
+T = 1.0      # Time to maturity
+N = 1000     # Number of steps
+
+time, S = simulate_euler_black_scholes(S0, mu, sigma, T, N)
+
+plt.figure(figsize=(12, 6))
+plt.plot(time, S)
+plt.title(f"Euler Approximation of BS")
+plt.ylabel("Time")
+plt.xlabel("Asset price")
+plt.savefig("results/euler", dpi=300, bbox_inches="tight")
--- src/compfin/volatility.py
+++ src/compfin/volatility.py
@@ -1,22 +1,25 @@
-"""Market volatility calculations."""
+"""
+Course: Computational Finance
+Names: Marcell ..., Michael ... and Tika van Bennekum
+Student IDs: ..., ... and 13392425
 
-import datetime
+File description:
+    Part 1 of lab assignment 1.
+    In this file we determine the volatility of a certain stock in the markt
+    within a certain time. We do this with a classical estimator and a
+    Parkinson estimator and compare them. We also compare the results against
+    the implied volatility of the same stock.
+"""
 
-import matplotlib.pyplot as plt
-import numpy as np
 import yfinance as yf
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 from skfolio.datasets import load_sp500_dataset, load_sp500_implied_vol_dataset
 from skfolio.preprocessing import prices_to_returns
 
-"""
-Notes:
-    tickers:
-    "AAPL" is for Apple Inc.
-    "MSFT" is for Microsoft
-    "GOOGL" is for Alphabet (Google)
-    "^GSPC" is for the S&P 500 index
-    "TSLA" is for Tesla
-"""
+
 ticker = "TSLA"
 start_date = "2015-01-01"
 end_date = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -28,61 +31,104 @@ prices_close = data["Close"]
 prices_high = data["High"]
 prices_low = data["Low"]
 
-# Equation 9 and 14 from app A
-# since the timesteps are 1, we can use python mean and std formula
-returns = (prices_close - prices_close.shift(1)) / prices_close.shift(1)
-mean_return = returns.mean()
-volatility = returns.std()
 
-# alternative (parkinson) volatility
-inside_sum_parkinson = np.log(prices_high / prices_low) ** 2
-constant_parkinson = 1 / 4 * np.log(2)
-volatility_parkinson = np.sqrt(constant_parkinson * inside_sum_parkinson.sum())
+def classical_drift(returns):
+    """Formula for natural estimator of the drift parameter mu (Eq 9)."""
+    return returns.sum() / len(returns)
 
 
-def rolling_window_comparison():
-    """Compares the volatilities by applying rolling window."""
+def classical_volatility(returns):
+    """This is the formula of the classical estimator to calculate volatility (Eq 14).
+    In our case, because T=N, it is the same as the std formula from python."""
+    returns_squared = returns**2
+    mean_return = classical_drift(returns)
+    N = len(returns)
+    T = N  # timestep is per day, so T = N
+    volatility_squared = (1 / (N - 1)) * returns_squared.sum() - (
+        T / (N - 1)
+    ) * mean_return**2
+    volatility = np.sqrt(volatility_squared)
+    return volatility
+
+
+def parkinson_volatility(high_prices, low_prices):
+    """Parkinson estimator for volatility."""
+    inside_sum = np.log(high_prices / low_prices) ** 2
+    constant = 1 / (4 * np.log(2))
+    volatility = np.sqrt(constant * inside_sum.sum())
+    return volatility
+
+
+def rolling_window_comparison(returns, prices_high, prices_low):
+    """Comparison of a rolling window using a Classical estimator versus a Parkinson estimator."""
     size_window = 30
-    rolling_window = returns.rolling(window=size_window).std()
+
+    # Parkinson rolling window
+    inside_sum_parkinson = np.log(prices_high / prices_low) ** 2
+    constant_parkinson = 1 / (4 * np.log(2))
     sum_parkinson_window = inside_sum_parkinson.rolling(window=size_window).sum()
     rolling_window_parkinson = np.sqrt(constant_parkinson * sum_parkinson_window)
 
+    # Classical rolling window
+    rolling_window_classical = returns.rolling(window=size_window).apply(
+        classical_volatility, raw=True
+    )
+
     plt.figure(figsize=(12, 6))
-    rolling_window.plot(label="Rolling volatility classical")
+    rolling_window_classical.plot(label="Rolling volatility classical")
     rolling_window_parkinson.plot(label="Rolling volatility Parkinson")
     plt.legend()
     plt.title("Rolling volatility: classical vs Parkinson (30 days)")
     plt.ylabel("Volatility")
     plt.xlabel("Date")
-    plt.show()
-
-
-# rolling_window_comparison()
+    plt.savefig("results/classical_vs_parkinson.png", dpi=300, bbox_inches="tight")
 
 
 def realized_vs_implied():
-    """Compares the realised and implied volatilities."""
+    """Plots the implied volatility against the classical and Parkinson realized volatility."""
+    size_window = 30
+
     # Load historical price data and implied volatility data
-    # TODO: results look weird, fix it
-    # TODO: add the parkinson estimator
     prices = load_sp500_dataset()
     stock = "GE"  # GE is a random stock from S&P 500
 
+    # Implied volatility
     implied_vol = load_sp500_implied_vol_dataset()["2015":]
     implied_vol = implied_vol[stock]
 
+    # Realized classical volatility
     X = prices_to_returns(prices)
     X = X.loc["2015":][stock]
-    realized_vol = X.rolling(window=30).std()
+    realized_vol_clas = X.rolling(window=size_window).apply(
+        classical_volatility, raw=True
+    )
+
+    # Realized Parkinson volatility
+    extra_data = yf.download(stock, start="2015-01-01", end="2022-12-31")
+    prices_high = extra_data["High"]
+    prices_low = extra_data["Low"]
+
+    inside_sum_parkinson = np.log(prices_high / prices_low) ** 2
+    constant_parkinson = 1 / (4 * np.log(2))
+    sum_parkinson_window = inside_sum_parkinson.rolling(window=size_window).sum()
+    realized_vol_park = np.sqrt(constant_parkinson * sum_parkinson_window)
 
     plt.figure(figsize=(12, 6))
-    plt.plot(realized_vol, label="Realized Volatility")
+    plt.plot(realized_vol_clas, label="Realized Volatility (classical)")
     plt.plot(implied_vol, label="Implied Volatility")
+    plt.plot(realized_vol_park, label="Realized Volatility (Parkinson)")
     plt.legend()
-    plt.title(f"Realized vs. Implied Volatility for {stock}")
+    plt.title(f"Realized vs. implied Volatility for {stock}")
     plt.ylabel("Volatility")
     plt.xlabel("Date")
-    plt.show()
+    plt.savefig("results/realized_vs_implied", dpi=300, bbox_inches="tight")
 
 
+# Alternative (parkinson) volatility
+volatility_parkinson = parkinson_volatility(prices_high, prices_low)
+# Rolling window comparison
+returns = (prices_close - prices_close.shift(1)) / prices_close.shift(1)
+rolling_window_comparison(returns, prices_high, prices_low)
+# Implied versus realized volatility
 realized_vs_implied()
+
